d9c1bac91b0fc9eb48bf1ab4df7c99b2a4ced335
Tomohiko Takahashi <takahashi@tomohiko.io>
2022-05-22 20:42:30 +0900

    処理を整理

--- lambda/index.ts
+++ lambda/index.ts
@@ -7,62 +7,43 @@ export const handler: EmptyHandler = async function () {
 
     const db = new DynamoDB.DocumentClient();
     const tableName = process.env.TABLE_NAME;
-    const param = {
+
+    // リツイートした最後のツイートIDを取得
+    const getParam = {
         TableName: tableName,
-        Item: {
+        Key: {
             type: "previousLoadPosition",
-            value: "aaa",
         },
     };
-    db.put(param, function (err: any, data: any) {
-        if (err) {
-            console.log(err);
-        } else {
-            db.scan({ TableName: tableName }, function (err: { code: any; }, data: any) {
-                if (err) {
-                    console.log("error", err.code);
-                } else {
-                    console.log("Table Items: ", data);
-                }
-            });
-        }
-    });
+    const data = await db.get(getParam).promise();
+    const previousId = data.Item['value'];
+    console.log('previousID: ' + previousId);
 
+    // Twitterのトークンを取得
     const ssm = new SSM();
     const params = {
         Name: '/shinkansen-ice/twitter',
         WithDecryption: true,
     };
-    console.log('------------1');
     const token = await ssm.getParameter(params).promise();
-    console.log('------------2');
-    console.log(token.Parameter.Value);
+    const bearerToken = token.Parameter.Value;
+    console.log('token: ' + bearerToken);
 
-
-    // Search for Tweets within the past seven days
-    // https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
-
+    // ツイート検索
     const needle = require('needle');
-
     const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
-
     async function getRequest() {
-
-        // Edit query parameters below
-        // specify a search query, and any additional fields that are required
-        // by default, only the Tweet ID and text fields are returned
         const params = {
             'query': '#シンカンセンスゴクカタイアイス -is:retweet has:media',
             'tweet.fields': 'author_id'
         }
-
         const res = await needle('get', endpointUrl, params, {
             headers: {
                 "User-Agent": "v2RecentSearchJS",
-                "authorization": `Bearer ${token.Parameter.Value}`
+                "authorization": `Bearer ${bearerToken}`,
+                "since_id": previousId,
             }
         })
-
         if (res.body) {
             return res.body;
         } else {
@@ -71,15 +52,26 @@ export const handler: EmptyHandler = async function () {
     }
 
     (async () => {
-
         try {
-            // Make request
+            // ツイート検索実行
             const response = await getRequest();
             console.dir(response, {
                 depth: null
             });
+            // 結果がある場合
+            if (response.data[0]) {
+                // ツイートIDを保存
+                const putParam = {
+                    TableName: tableName,
+                    Item: {
+                        type: "previousLoadPosition",
+                        value: response.data[0],
+                    },
+                };
+                db.put(putParam).promise();
+            }
             return JSON.stringify({
-                data: console.dir(response, { depth: null })
+                data: console.dir(response.data, { depth: null })
             });
 
         } catch (e) {
